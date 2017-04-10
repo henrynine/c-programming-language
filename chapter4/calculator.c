340c73b9fd4a8866a817e2f154f5b6c0c00c621d
Henry Middleton <h78900@gmail.com>
2017-04-10 18:28:10 -0400

    Add modulus function to calculator

--- chapter4/calculator.c
+++ chapter4/calculator.c
@@ -37,6 +37,13 @@ int main()
       else
         printf("error: zero divisor\n");
       break;
+    case '%':
+      op2 = pop();
+      if (op2 != 0.0)
+        push(((int)pop()) % ((int)(op2)));
+      else
+        printf("error: zero divisor\n");
+      break;
     case '\n':
       printf("\t%.8g\n", pop());
       break;
